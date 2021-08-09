b45a28f6e18ff4615a8a3d1b9af30d6c8e89f58f
G Durga Devi <G Durga Devi@DESKTOP-7P2QHB6>
2021-08-09 19:59:09 +0800

    update(add part)

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,39 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	 ArrayList<Category> categoryList = new ArrayList<Category>();
+	
+	
+		categoryList.add(new Category("CC001", "Western"));
+		categoryList.add(new Category("CC002", "Asian"));
+		categoryList.add(new Category("CC003", "Vegetarian"));
+		
+		
 
+	}
+
+	
+	public static Category inputCategory() {
+		String ID = Helper.readString("Enter asset tag > ");
+		String Name = Helper.readString("Enter description > ");
+		
+
+		Category cc= new Category(ID, Name);
+		return cc;
+		
+	}
+	public static void addCamcorder(ArrayList<Category> camcorderList, Category cc) {
+		
+		camcorderList.add(cc);
+		System.out.println("Category added");
+	}
+
+	public static void addCategory(ArrayList<Category> categoryList, Category c1) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,14 +1,32 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
-
+	private Category c1;
+	private Category c2;
+	private Category c3;
+	
+	private ArrayList<Category> categoryList;
+	
+	
 	@Before
 	public void setUp() throws Exception {
+		// prepare test data
+		c1 = new Category("CC0011", "Nikon HDSLR");
+		c2 = new Category("CC0012", "Sony DSC-RX100M7");
+		c3 = new Category("CB0011", "My Google Chromebook 1st");
+		
+
+		categoryList= new ArrayList<Category>();
+		
 	}
+
+
 
 	@After
 	public void tearDown() throws Exception {
@@ -19,5 +37,30 @@ public class C206_CaseStudyTest {
 		//fail("Not yet implemented"); 
 		assertTrue("C206_CaseStudy_SampleTest ",true);
 	}
+		
+		
+		
+	
+		@Test
+		public void testAddCategory() {
+			//fail("Not yet implemented");
+			// write your code here
+			
+			// Item list is not null, so that can add a new item
+			assertNotNull("Test if there is valid Chromebook arraylist to add to", categoryList);
+					
+			//Given an empty list, after adding 1 item, the size of the list is 1
+			C206_CaseStudy.addCategory(categoryList, c1);						
+			assertEquals("Test if that Chromebook arraylist size is 1?", 1, categoryList.size());
+			
+			//The item just added is as same as the first item of the list
+			assertSame("Test that Chromebook is added same as 1st item of the list?", c1, categoryList.get(0));
+					
+			//Add another item. test The size of the list is 2?
+			C206_CaseStudy.addCategory(categoryList, c2);
+			assertEquals("Test that Chromebook arraylist size is 2?", 2, categoryList.size());
+		}
+	
+	}
 
-}
+
